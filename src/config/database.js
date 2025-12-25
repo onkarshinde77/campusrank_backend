be3b74ea7fa4df819f5598cb74d171f527cbd054
onkarshinde77 <shindeonkar707@gmail.com>
2025-12-25 09:48:45 +0530

    fixing bugs of github hitmap

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGODB_URI);
+    // console.log(process.env.MONGODB_URI);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
